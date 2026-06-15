bf7d831f983d79d77532ad78f6375d19c8991495
eoblackzwkj <143085633+blackzww@users.noreply.github.com>
2026-06-15 10:59:17 -0300

    Update loader.lua

--- Mirrors Hub - Loader/loader.lua
+++ Mirrors Hub - Loader/loader.lua
@@ -1,10 +1,24 @@
-
 local scripts = {
-    [142823291] = "https://raw.githubusercontent.com/SEUUSER/SEUREPO/main/mm2.lua", -- MM2
+    [142823291] = "https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20MM2/mm2.lua", -- MM2
 }
 
 local universal = "https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20Universal/universal.lua"
 
-local url = scripts[game.PlaceId] or universal
+local supported = scripts[game.PlaceId]
+local url = supported or universal
+
+if supported then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Mirrors Hub",
+        Text = "Game Supported 🎮",
+        Duration = 5
+    })
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Mirrors Hub",
+        Text = "Game not Supported ⚠️ | Loading Universal",
+        Duration = 5
+    })
+end
 
 loadstring(game:HttpGet(url))()
