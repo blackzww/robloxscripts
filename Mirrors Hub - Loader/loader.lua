c38e5ea582a299d374cf2fb238b689d3a32c5efc
eoblackzwkj <143085633+blackzww@users.noreply.github.com>
2026-06-15 10:57:10 -0300

    Update loader.lua

--- Mirrors Hub - Loader/loader.lua
+++ Mirrors Hub - Loader/loader.lua
@@ -3,7 +3,7 @@ local scripts = {
     [142823291] = "https://raw.githubusercontent.com/SEUUSER/SEUREPO/main/mm2.lua", -- MM2
 }
 
-local universal = "https://raw.githubusercontent.com/SEUUSER/SEUREPO/main/universal.lua"
+local universal = "https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20Universal/universal.lua"
 
 local url = scripts[game.PlaceId] or universal
 
