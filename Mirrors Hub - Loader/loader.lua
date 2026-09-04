17b0276c748537ed5778b0f6abc538efdddc9c15
blackzw.mp3 <143085633+blackzww@users.noreply.github.com>
2026-09-04 09:17:22 -0300

    Update loader.lua

--- Mirrors Hub - Loader/loader.lua
+++ Mirrors Hub - Loader/loader.lua
@@ -2,17 +2,19 @@ local scripts={
 	[12308344607]="https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20Doors%20(Lobby)/doorsl.lua",
 	[6516141723]="https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20Doors%20(Lobby)/doorsl.lua",
 	[6524124388]="https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20Doors%20(Lobby)/doorsl.lua",
-	[136801880565837]="https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20%5BFPS%5D%20Flick/flick.lua"
+	[136801880565837]="https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20%5BFPS%5D%20Flick/flick.lua",
+	[142823291]="https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20MM2/mm2.lua"
 }
 
 local games={
-	[2440500124]="SEM SCRIPTS ATUALMENTE"
+	[2440500124]=false
 }
 
 local universal="https://raw.githubusercontent.com/blackzww/robloxscripts/refs/heads/main/Mirrors%20Hub%20-%20Universal/universal.lua"
+local url=scripts[game.PlaceId]
+local supported=url~=nil
 
-local url=scripts[game.PlaceId] or games[game.GameId] or universal
-local supported=scripts[game.PlaceId]~=nil or games[game.GameId]~=nil
+if not url then url=universal end
 
 pcall(function()
 	game:GetService("StarterGui"):SetCore("SendNotification",{
@@ -22,18 +24,8 @@ pcall(function()
 	})
 end)
 
-local ok,source=pcall(game.HttpGet,game,url)
-
-if not ok or type(source)~="string" or source=="" then
-	warn("[MIRRORS] Failed to load script.")
-	return
-end
-
-local fn,err=loadstring(source)
-
-if not fn then
-	warn("[MIRRORS] Compile error: "..tostring(err))
-	return
-end
-
-fn()
+pcall(function()
+	local source=game:HttpGet(url)
+	local fn=loadstring(source)
+	if fn then fn() end
+end)
